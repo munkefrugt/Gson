c873e13aec9ec714e41c346a4b78f55c044b03ad
munkefrugt@gmail.com <munkefrugt@gmail.com>
2016-04-27 20:27:24 +0200

    færdig virker

--- app/src/main/java/com/example/gson/CustomAdapter.java
+++ app/src/main/java/com/example/gson/CustomAdapter.java
@@ -5,6 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -12,26 +16,30 @@ import java.util.List;
  * Created by v on 4/27/16.
  */
 
-// implements methods that belongs to base adapter.
+// implements methods that belongs to base adapter, to get the pre made methods.
 public class CustomAdapter extends BaseAdapter {
 
     // in the video. https://www.youtube.com/watch?v=oZpv6W3Lflo
     // its  List<Response.MovieItem>, but the Api has been changed since. so now its List<Response.MoviesBean>
     // it can be found inside the Response class. therefore (Response.MoviesBean)
-    private List<Response.MoviesBean> mMoovieitem;
+    private List<Response.MoviesBean> mMovieitem;
     private Context mContext;
     private LayoutInflater inflater;
 
-
+    public CustomAdapter( Context mContext, List<Response.MoviesBean> mMovieitem) {
+        this.mContext = mContext;
+        this.mMovieitem = mMovieitem;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        // we changed the return value to the one below.
+        return mMovieitem.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mMovieitem.get(position);
     }
 
     @Override
@@ -41,6 +49,29 @@ public class CustomAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        // what does this mean?
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // why parent?, and why false?
+        View rowView = inflater.inflate(R.layout.each_list_item, parent, false);
+
+        // item is a type Response.MoviesBean, wich means we can what exactly??? read more on Gson..
+
+        // Response.MoviesBean -- MoviesBean er inner class i Response.
+        Response.MoviesBean item = (Response.MoviesBean) getItem(position);
+
+        ImageView thumbnail = (ImageView) rowView.findViewById(R.id.thumbnail);
+        TextView title = (TextView) rowView.findViewById(R.id.title);
+        TextView rating = (TextView) rowView.findViewById(R.id.rating);
+
+        // here the magic happens..Here it gets set to the response.
+        // her getPosters() er en metode i Response.MoviesBean, og getOriginal er så en methode der der ikke kan sees i classen . men er indbygget som en valgmulighed.
+        // eller en methode fra bilioteket..
+        String imageUrl = item.getPosters().getOriginal();
+        Picasso.with(mContext).load(imageUrl).into(thumbnail);
+        title.setText(item.getTitle());
+        rating.setText(item.getRatings().getAudience_rating());
+        return rowView;
     }
 }
--- app/src/main/java/com/example/gson/MainActivity.java
+++ app/src/main/java/com/example/gson/MainActivity.java
@@ -5,8 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    Response responseObj;
+    CustomAdapter adapter;
+    String url = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=9htuhtcb4ymusd73d4z6jxcj";
+    Gson gson;
+    AsyncHttpClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +30,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        listView = (ListView) findViewById(R.id.moovielist);
+        // instatiate client.
+        client = new AsyncHttpClient();
+
+        // when you fill out this one, it auto maticly creates the to inner overidden methods.
+        client.get(MainActivity.this, url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String responestr = new String(responseBody);
+
+                // instatiate gson object.
+                gson = new Gson();
+                responseObj = gson.fromJson(responestr,Response.class);
+                adapter = new CustomAdapter(MainActivity.this, responseObj.getMovies());
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
 
     }
 
