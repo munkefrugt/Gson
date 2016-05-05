e952aee066097b48edc6f425379402ea5029150e
munkefrugt@gmail.com <munkefrugt@gmail.com>
2016-05-05 23:39:30 +0200

    nu prøver jeg at slette customAdapter

--- app/src/main/java/com/example/gson/MainActivity.java
+++ app/src/main/java/com/example/gson/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
 
-                // lad være med at ændre på "Extra String",
+                // lad være med at ændre på det der står inde i "", det skal være det samme i modtager activitien. ("startAdress",startAdress)
 
                 intent.putExtra("startAdress",startAdress);
                 intent.putExtra("arrival_time",arrival_time);
@@ -90,8 +90,8 @@ public class MainActivity extends AppCompatActivity {
                 arrival_time = String.valueOf(responseObj.getRoutes().get(0).getLegs().get(0).getArrival_time().getValue());
 
                 Log.i("startAdress", startAdress);
-                adapter = new CustomAdapter(MainActivity.this, responseObj.getRoutes());
-                listView.setAdapter(adapter);
+                //adapter = new CustomAdapter(MainActivity.this, responseObj.getRoutes());
+                //listView.setAdapter(adapter);
             }
 
             @Override
